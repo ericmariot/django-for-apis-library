37a04ba2a08318fc1abdabb00bdc9d65356ea783
Eric Mariot <ericmariots@gmail.com>
2021-09-01 20:42:39 -0300

    first commit

---
+++ books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Book(models.Model):
+  title = models.CharField(max_length=250)
+  subtitle = models.CharField(max_length=250)
+  author = models.CharField(max_length=100)
+  isbn = models.CharField(max_length=13) # is a unique 13-character identifier to every published book
+
+  def __str__(self):
+    return self.title
